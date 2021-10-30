cb9abe53bdf874bab99862ac87faf9d40128cda8
wjuang <williamsjuang@gmail.com>
2021-10-29 22:52:08 -0400

    changed gameID to gameName for comments

--- models/Comment.js
+++ models/Comment.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const commentSchema = new mongoose.Schema({
     name: {type: String, default: 'Anonymous'},
-    comment: {type: String, required: true},  
-    gameId: {type: Number}
+    comment: {type: String, required: true},
+    gameName: {type: String}
 })
 
 const Comment = mongoose.model('Comment', commentSchema)
